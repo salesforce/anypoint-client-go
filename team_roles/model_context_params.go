5309d31780471bc9df675c41c6de5b7031d7922d
pipeline-cloudhub-clients-generator <pipeline@cloudhub-client-generator.com>
2021-08-25 08:32:04 +0000

    Generates modules from pipeline.

--- team_roles/model_context_params.go
+++ team_roles/model_context_params.go
@@ -17,6 +17,7 @@ import (
 // ContextParams struct for ContextParams
 type ContextParams struct {
 	Org *string `json:"org,omitempty"`
+	EnvId *string `json:"envId,omitempty"`
 }
 
 // NewContextParams instantiates a new ContextParams object
@@ -68,10 +69,45 @@ func (o *ContextParams) SetOrg(v string) {
 	o.Org = &v
 }
 
+// GetEnvId returns the EnvId field value if set, zero value otherwise.
+func (o *ContextParams) GetEnvId() string {
+	if o == nil || o.EnvId == nil {
+		var ret string
+		return ret
+	}
+	return *o.EnvId
+}
+
+// GetEnvIdOk returns a tuple with the EnvId field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *ContextParams) GetEnvIdOk() (*string, bool) {
+	if o == nil || o.EnvId == nil {
+		return nil, false
+	}
+	return o.EnvId, true
+}
+
+// HasEnvId returns a boolean if a field has been set.
+func (o *ContextParams) HasEnvId() bool {
+	if o != nil && o.EnvId != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetEnvId gets a reference to the given string and assigns it to the EnvId field.
+func (o *ContextParams) SetEnvId(v string) {
+	o.EnvId = &v
+}
+
 func (o ContextParams) MarshalJSON() ([]byte, error) {
 	toSerialize := map[string]interface{}{}
 	if o.Org != nil {
 		toSerialize["org"] = o.Org
+	}
+	if o.EnvId != nil {
+		toSerialize["envId"] = o.EnvId
 	}
 	return json.Marshal(toSerialize)
 }
